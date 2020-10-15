b49d568c12cebd1110bf5e69dc409da52a28263a
TAPASYA1234 <31138915+TAPASYA1234@users.noreply.github.com>
2020-10-15 16:07:38 +0530

    Revert "try again"

--- src/hack.js
+++ src/hack.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class hack extends Component {
     render() {
         return (
-            <div><p>tried again</p>
+            <div>
                 
             </div>
         )
